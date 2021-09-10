730c3cac8bc311ca93814d69e76545d135346441
PanoccaJhon <jpanoccam@unsa.edu.pe>
2021-09-10 17:21:56 -0500

    tutorial hasta formView

---
+++ django-tutorial/app/store/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from store.views.category.views import *
+
+
+app_name = 'store'
+
+
+urlpatterns = [
+    path('category/list/', CategoryListView.as_view(), name='category_list'),
+    path('category/add/', CategoryCreateView.as_view(), name='category_create'),
+    path('category/edit/<int:pk>/', CategoryUpdateView.as_view(), name='category_update'),
+    path('category/delete/<int:pk>/', CategoryDeleteView.as_view(), name='category_delete'),
+    path('category/form/', CategoryFormView.as_view(), name='category_form'),
+]
